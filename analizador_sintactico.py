77d1d0817e786dd2699b559bc252d3ace87586c5
Alejandra V <134009912+Malejav18@users.noreply.github.com>
2025-05-06 22:38:13 -0500

    Update analizador_sintactico.py

--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -60,8 +60,9 @@ productions = {
     'if_tail': [['elif', 'condition', 'tk_dos_puntos', 'loop_block', 'if_tail'], ['else', 'tk_dos_puntos', 'loop_block'],[]],
     'while_stmt': [['while', 'condition', 'tk_dos_puntos', 'loop_block', 'while_tail']],  # Instrucción while
     'while_tail': [['else', 'tk_dos_puntos', 'loop_block'],[]],
+    
     'for_stmt': [['for', 'id', 'in', 'loop_iterable', 'tk_dos_puntos', 'loop_block']],  # Instrucción for
-    'loop_iterable': [['range', 'tk_par_izq', 'num_list', 'tk_par_der'], ['tk_corchete_izq', 'items_array', 'tk_corchete_der'], ['id', 'factor_tail'], ['self', 'factor_tail'], ['tk_par_izq', 'items_tuple', 'tk_par_der']],  # Rango de números o ID
+    'loop_iterable': [['range', 'tk_par_izq', 'items_tuple', 'tk_par_der'], ['tk_corchete_izq', 'items_array', 'tk_corchete_der'], ['id', 'factor_tail'], ['self', 'factor_tail'], ['tk_par_izq', 'items_tuple', 'tk_par_der']],  # Rango de números o ID
     
     'try_stmt': [['try', 'tk_dos_puntos', 'loop_block', 'try_stmt_tail']], # try: except: finally:
     'try_stmt_tail': [['except_stmt', 'finally_stmt']],
@@ -81,7 +82,7 @@ productions = {
     'condition_tail': [['in', 'id'], ['comp_op', 'expr'], []],
     'comp_op': [['tk_igual'], ['tk_distinto'], ['tk_menor'], ['tk_mayor'], ['tk_menor_igual'], ['tk_mayor_igual']],
     'expr': [['term', 'expr_']],
-    'expr_': [['tk_suma', 'term', 'expr_'], 
+    'expr_': [['tk_suma', 'term', 'expr_'],
               ['tk_resta', 'term', 'expr_'],
               ['tk_modulo', 'term', 'expr_'], 
               ['and', 'term', 'expr_'],  
